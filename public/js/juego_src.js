12a6c3dfe98da8e6ded0045c050dca366ebfbb19
christian-2001 <villegaschristian16@gmail.com>
2025-04-27 01:10:35 -0300

    Añadido temporizador para el juego
    -Añadido temporizador teniendo un total de 10 segundos
    -Logica añadida cuando el temporizador se detiene y se reinicia al elegir respuesta, o no responder pasados los 10 segundos

--- public/js/juego_src.js
+++ public/js/juego_src.js
@@ -1,9 +1,11 @@
+let temporizador = 10
+let conteo
 document.addEventListener("DOMContentLoaded", () => {
     mostrarPregunta()
 });
 
 const mostrarPregunta = () => {
-
+    temporizador = 10
     let buttons = ''
     let item = ''
 
@@ -43,14 +45,33 @@ const mostrarPregunta = () => {
         })
     }
 
+    document.getElementById('temporizador').innerHTML = `<h1> 0:10 </h1>`
+
     document.querySelector('.pregunta').innerHTML = ` ${h1_pregunta} ${item} `
 
     document.getElementById('pregunta_contador').innerHTML = `Preguntas ${index + 1}/10`
     
     document.querySelector('.btn_opciones').innerHTML = `${buttons}`
 
-}
 
+
+    conteo = setInterval(() => {
+        temporizador--
+        document.getElementById('temporizador').innerHTML = `0:0${temporizador}`
+        if(temporizador == 0) {
+            document.querySelector('.puntos').innerHTML = `<h1> Tiempo fuera!!! </h1>`
+            const botones = document.querySelectorAll('button')
+            botones.forEach(btn => {
+                btn.disabled = true
+                btn.classList.remove('hover:-translate-y-1')
+                btn.classList.remove('hover:bg-gray-400')
+            })
+            document.querySelectorAll('button').forEach(btn => btn.disabled = true)
+            clearInterval(conteo)
+            avanzar()
+        }
+    }, 1000)
+}
 
 const avanzar = () => {
     index++
@@ -71,7 +92,7 @@ const mostrar_resultados = () => {
         <div class='w-fit ml-auto mr-auto mt-60 border-4 bg-white p-20'>
             <h1 class='text-center'> Resultados de la partida </h1>
             <h1 class='text-left mt-10'> Puntaje final: 0 </h1>
-            <h1 class='text-left mt-5'> Tiempo promedio en cada respuesta: 5 segundos </h1>
+            <h1 class='text-left mt-5'> Tiempo promedio en cada respuesta: 0 segundos </h1>
         </div>
     `
 }
@@ -79,7 +100,7 @@ const mostrar_resultados = () => {
 //Cuando se elije la respuesta correcta, se ejecuta la siguiente funcion
 const resp_correcta = () =>{
 
-
+    clearInterval(conteo)
 
     //Obtiene el elemento button, se pinta de color verde y deshabilita sus clases
     const btn_green = document.getElementById('green')
@@ -102,8 +123,8 @@ const resp_correcta = () =>{
 
 //Cuando se elije una de las respuestas incorrectas, se ejecuta la siguiente funcion
 const resp_incorrectas = () =>{
-
-
+    
+    clearInterval(conteo)
 
     //Guardo los buttons con la clase '.red'
     const btn_red = document.querySelectorAll('.red')
